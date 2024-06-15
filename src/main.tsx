b42fdbdcd14dbb9fcacd0a70645341aae7875a1e
Joselu <joseluissgz97@gmail.com>
2024-06-15 22:42:00 +0200

    add: mian and vite-end.d.ts

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import UsersMeno from './03-useMemo/UsersMeno';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  <UsersMeno />
+  // </React.StrictMode>
+);
