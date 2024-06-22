711ec06ad0f02ec8c21b84e5d79525dba6fddea1
Joselu <joseluissgz97@gmail.com>
2024-06-22 10:26:23 +0200

    chore: Update Jest configuration to include moduleNameMapper and transform options

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 };
