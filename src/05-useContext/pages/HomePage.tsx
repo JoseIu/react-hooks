6278048693c4d5bf52919d723a564c2445b01894
Joselu <joseluissgz97@gmail.com>
2024-06-22 10:26:32 +0200

    feat: Update HomePage to display user name

--- src/05-useContext/pages/HomePage.tsx
+++ src/05-useContext/pages/HomePage.tsx
@@ -3,5 +3,10 @@ import { useUser } from '../hook/useUser';
 export const HomePage = () => {
   const { user } = useUser();
   console.log(user);
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <span aria-label="user">{user.name} </span>
+    </div>
+  );
 };
